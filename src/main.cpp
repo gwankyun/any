a8bbeec14243c34e4321755974d2e1235c880bad
權少 <ljqican@163.com>
2019-12-08 22:43:40 +0800

    添加emplace八參數重載

--- src/main.cpp
+++ src/main.cpp
@@ -227,6 +227,9 @@ int main(int argc, char* argv[])
 
         o1.emplace<Object>(0, 1, 1, 1, 1, 1, 1);
         test<Object>("emplace 7", o1);
+
+        o1.emplace<Object>(0, 1, 1, 1, 1, 1, 1, 1);
+        test<Object>("emplace 8", o1);
     }
     return 0;
 }
